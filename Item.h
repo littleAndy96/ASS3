894f093f3f265187c5b8aa46edb92bac18f97407
mchamby <53239708+mchamby@users.noreply.github.com>
2019-07-24 14:57:06 -0400

    Update Item.h

--- Item.h
+++ Item.h
@@ -1,16 +1,26 @@
-class Item{
-    public:
-        Item();
-        Item(double itemPrice,double taxRate); 
-        ~Item() = default; // default deconstructor 
-        static void setTaxRate(double taxRate); // sets the tax rate of the item
-        void setItemPrice(double itemPrice); // sets the price of the item
-        static double getTaxRate(); // gets the tax rate
-        double getItemPrice() const; // gets the price of the item
-        double getItemTaxed(); // gets the total of the item * taxes
-        double getTaxOfItem(); // gets the taxes applied on the item
-    private:
-        static double taxRate; // rate of the taxes
-        double itemPrice; // price of an item
+#pragma once
+
+#include <string>
+
+using namespace std;
+
+class Item {
+public:
+	Item(int id);
+	Item(int id, double itemPrice, string name);
+	~Item() = default; // default deconstructor 
+	static void setTaxRate(double taxRate); // sets the tax rate of the item
+	void setItemPrice(double itemPrice); // sets the price of the item
+	static double getTaxRate(); // gets the tax rate
+	int getItemId() const;
+	double getItemPrice() const; // gets the price of the item
+	double getItemTaxed(); // gets the total of the item * taxes
+	double getTaxOfItem(); // gets the taxes applied on the item
+	string getItemName(); // returns the product name
+private:
+	const int id; // item id: can't be changed
+	const string productName; // also known as the product description
+	static double taxRate; // rate of the taxes
+	double itemPrice; // price of an item
 
 };
