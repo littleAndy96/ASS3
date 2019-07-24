0c90a7c4df9deae1f0e9f5488e1fde99fe48eefd
mchamby <53239708+mchamby@users.noreply.github.com>
2019-07-24 14:58:10 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,30 @@
 #include "Item.h"
+#include "Inventory.h"
 
 #include <iostream>
 
 using namespace std;
 
 int main() {
-    
-    Item* item1 = new Item(100,0.15);
+	Inventory* inventory = new Inventory();
+	Item* item1 = new Item(10011, 3.50, "Watermelon");
+	Item* item2 = new Item(10012, 3.50, "Milk");
+	Item* item3 = new Item(10013, 4.50, "Bag of bagels");
 
-    cout << item1->getItemPrice() << endl;
-    cout << item1->getTaxRate() << endl;
-    
-    delete item1;
-    
-    return 0;
+	cout << item1->getItemPrice() << endl;
+	cout << item1->getTaxRate() << endl;
+
+	inventory->addItem(*item1, 11);
+	inventory->itemCheck(*item1);
+	inventory->addItem(*item2, 5);
+	inventory->addItem(*item2, 1);
+	inventory->itemCheck(*item2);
+	inventory->removeItem(*item2, 7);
+	inventory->removeItem(*item3, 1);
+
+
+	delete item1;
+	delete inventory;
+
+	return 0;
 }
