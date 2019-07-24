5a67c958bec96cd1e42700ca0c77f710c9fff5c1
mchamby <53239708+mchamby@users.noreply.github.com>
2019-07-24 14:56:08 -0400

    Update Item.cpp

--- Item.cpp
+++ Item.cpp
@@ -1,32 +1,42 @@
 #include "Item.h"
 
+using namespace std;
 
 double Item::taxRate = 0.15;
 
-Item::Item(){
-        itemPrice = 0;
+Item::Item(int id) : id(id) {
+	itemPrice = 0;
 }
-Item::Item(double itemPrice,double taxRate){
-        this->itemPrice = itemPrice;
-        this->taxRate = taxRate;
+Item::Item(int id, double itemPrice, string name) : id(id), productName(name) {
+	this->itemPrice = itemPrice;
+	this->taxRate = taxRate;
 }
-void Item::setItemPrice(double itemPrice){
-    this->itemPrice = itemPrice;
+void Item::setItemPrice(double itemPrice) {
+	this->itemPrice = itemPrice;
 }
-void Item::setTaxRate(double taxRate){
-    Item::taxRate = taxRate;
+void Item::setTaxRate(double taxRate) {
+	Item::taxRate = taxRate;
 }
-double Item::getItemPrice() const{
-    return this->itemPrice;
+double Item::getItemPrice() const {
+	return this->itemPrice;
 }
-double Item::getTaxRate(){
-    return Item::taxRate;
+double Item::getTaxRate() {
+	return Item::taxRate;
 }
-double Item::getItemTaxed(){
-    double total;
-    total = this->itemPrice * this->taxRate;
-    return total;
+int Item::getItemId() const
+{
+	return this->id;
 }
-double Item::getTaxOfItem(){
-    return this->itemPrice * this->taxRate;
+double Item::getItemTaxed() {
+	double total;
+	total = this->itemPrice * this->taxRate;
+	return total;
+}
+double Item::getTaxOfItem() {
+	return this->itemPrice * this->taxRate;
+}
+
+string Item::getItemName()
+{
+	return this->productName;
 }
