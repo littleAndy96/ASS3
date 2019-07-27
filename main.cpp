f72fc1a469e7fc817e39cb87b656bda3a28851d3
mchamby <53239708+mchamby@users.noreply.github.com>
2019-07-26 21:58:44 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include "Item.h"
 #include "Inventory.h"
+#include "Payment.h"
+#include "ApprovalAuthority.h"
 
 #include <iostream>
 
 using namespace std;
 
 int main() {
+	ApprovalAuthority a1;
+	a1.checkApproval(); 
+	
+	//UNFINISHED
+
+	cout << '\n';
+
 	Inventory* inventory = new Inventory();
 	Item* item1 = new Item(10011, 3.50, "Watermelon");
 	Item* item2 = new Item(10012, 3.50, "Milk");
@@ -14,13 +23,13 @@ int main() {
 	cout << item1->getItemPrice() << endl;
 	cout << item1->getTaxRate() << endl;
 
-	inventory->addItem(*item1, 11);
-	inventory->itemCheck(*item1);
-	inventory->addItem(*item2, 5);
-	inventory->addItem(*item2, 1);
-	inventory->itemCheck(*item2);
-	inventory->removeItem(*item2, 7);
-	inventory->removeItem(*item3, 1);
+	inventory->addItem(item1, 11);
+	inventory->itemCheck(item1);
+	inventory->addItem(item2, 5);
+	inventory->addItem(item2, 1);
+	inventory->itemCheck(item2);
+	inventory->removeItem(item2, 7);
+	inventory->removeItem(item3, 1);
 
 
 	delete item1;
