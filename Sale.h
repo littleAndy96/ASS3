eccf27390951eaeb10d5638895940623f3efff5c
mchamby <53239708+mchamby@users.noreply.github.com>
2019-07-29 16:24:11 -0400

    Update Sale.h

--- Sale.h
+++ Sale.h
@@ -16,14 +16,15 @@ public:
 	void removeItemFromSale(Item*,int);
 	double calculateRunningTotalBeforeTaxes();
 	double calculateRunningTotalWithTaxes();
-	double calculateRunningTotalWithMethod(string method);
+	void calculateAmountChargedWithMethod(string method);
 	bool completeTransaction();
 private:
+	bool isComplete;
 	int id;
 	static int idCounter;
 	double runningTotalBeforeTaxes;
 	double runningTotalWithTaxes;
-	double amountCharged;
+	double amountCharged;	// the total amount after taking into account the payment type
 	Item** saleItems; // sale is an array of item pointers 
 	//Sale* completedSale = new Sale[maxSaleCapacity]; // array of completed sales moved to registry
 	static const int maxSaleCapacity = 1000;
